f34bfd908be0601b14dc0068a45dbf981ad33199
Dhia Aloui <61512421+Aloui183@users.noreply.github.com>
2020-03-13 17:12:03 +0100

    Create video.sql

---
+++ video.sql
@@ -0,0 +1,5 @@
+INSERT INTO Videos (id_Pays, id_Categorie, video_url, Titre, Description, date_de_sortie, date_ajout) VALUES 
+(1, 2, '1','Spenser', 'Film Action', '2020-02-15','20200313 10:52:59 AM'),
+(2, 2, '2','Expandables', 'Film Action', '2017-10-29','20180819 10:52:59 AM'),
+(3, 2, '3','Transporter', 'Film Action', '2015-11-14', '20160101 10:52:59 AM'),
+(4, 2, '4','Fast & Furious', 'Film Action', '2014-12-09', '20150619 10:52:59 AM')
