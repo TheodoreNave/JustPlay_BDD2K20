0b47693009856650273deecb2ecf826eda7940b7
Blind <55689277+BaretSacha@users.noreply.github.com>
2020-03-13 16:21:22 +0100

    Update and rename Villes-abonnement.sql to Villes-abonnement-Pays.sql

---
+++ Villes-abonnement-Pays.sql
@@ -0,0 +1,21 @@
+INSERT INTO Ville(Nom,Code_Postal) VALUES
+('Colombes',92700),
+('Paris',75000),
+('Puteaux',92800);
+
+
+
+INSERT INTO Abonnement VALUES (1,'Pack branleur',1,3.90)
+INSERT INTO Abonnement VALUES (2,'Pack couple',2,5.90)
+INSERT INTO Abonnement VALUES (3,'Pack famille',5,11.90)
+
+
+INSERT INTO Pays VALUES (1,'France')
+INSERT INTO Pays VALUES (2,'Italie')
+INSERT INTO Pays VALUES (3,'Russie')
+INSERT INTO Pays VALUES (4,'Angleterre')
+INSERT INTO Pays VALUES (5,'Japon')
+INSERT INTO Pays VALUES (6,'Espagnol')
+INSERT INTO Pays VALUES (7,'Chine')
+INSERT INTO Pays VALUES (8,'Allemagne')
+
