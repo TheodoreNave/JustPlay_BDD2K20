a9efa86423e4d8866621221473a73ebbbf08443f
OrioPrisco <adri.ringard@gmail.com>
2020-03-25 17:30:21 +0100

    paiement.sql

--- Paiement.sql
+++ Paiement.sql
@@ -1,6 +1,5 @@
-INSERT INTO Paiement (statut paiement)
-  VALUES
-    ('Pas effectué'),
-    ('En cours'),
-    ('Refusé'),
-    ('Accepté');
+INSERT INTO Paiement(statut_paiement, id_abonnement, id_utilisateur) VALUES
+('accepté', 1, 1),
+('en cours', 3, 2),
+('en cours', 2, 3),
+('refusé', 1, 4)
