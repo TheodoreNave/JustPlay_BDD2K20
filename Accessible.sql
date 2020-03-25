ecacdfcbb03d5c3dbf2d5d49c6af195ffcb14465
Théodore Nave <theodoreyws@hotmail.fr>
2020-03-25 17:43:42 +0100

    Added accesible + realise

---
+++ Accessible.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Accesible
+INSERT INTO Accesible (video_url, id_Pays) VALUES
+('1', '1'),
+('2', '2'),
+('3', '3'),
+('4', '4')
---
+++ realise.sql
@@ -0,0 +1,6 @@
+SELECT * FROM réalise
+INSERT INTO réalise (video_url, id_casting) VALUES
+('1', '1'),
+('2', '2'),
+('3', '3'),
+('4', '4')
