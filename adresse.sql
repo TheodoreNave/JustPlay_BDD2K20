edd8878a330ada29f431a44fcd929ba6dd60b377
OrioPrisco <adri.ringard@gmail.com>
2020-03-25 16:57:17 +0100

    adresses + id_adresse pour utilisateurs

---
+++ adresse.sql
@@ -0,0 +1,12 @@
+INSERT INTO Adresse(Lieu_de_residence, id_ville) VALUES
+('Adresse 1',1),
+('Adresse 2',2),
+('Adresse 3',3),
+('Sacha s Home',1),
+('Dhia s Home',2)
+
+UPDATE Utilisateurs set id_adresse =1 WHERE id_utilisateur = 1
+UPDATE Utilisateurs set id_adresse =2 WHERE id_utilisateur = 2
+UPDATE Utilisateurs set id_adresse =4 WHERE id_utilisateur = 3
+UPDATE Utilisateurs set id_adresse =3 WHERE id_utilisateur = 4
+UPDATE Utilisateurs set id_adresse =5 WHERE id_utilisateur = 5
